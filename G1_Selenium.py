cab5ab1cbe44adecd86d5b93b7c3e1342dc5e057
leonardosoler <leonardo-leoss@hotmail.com>
2021-10-03 00:48:45 -0300

    Pegar todas as noticias da pagina (sem o load do javascript) Titulo e subtitulo.

--- G1_Selenium.py
+++ G1_Selenium.py
@@ -9,15 +9,18 @@ content = response.content
 site = BeautifulSoup(content, 'html.parser')
 
 #HTML da noticia
-noticia = site.find('div', attrs={'class' : 'feed-post-body'} )
+noticias = site.findAll('div', attrs={'class' : 'feed-post-body'} )
 
-#Titulo
-titulo = noticia.find('a', attrs= {'class': 'feed-post-link'})
+for noticia in noticias:
+    #Titulo
+    titulo = noticia.find('a', attrs= {'class': 'feed-post-link'})
 
-subtitulo = noticia.find('a', attrs={'class': 'feed-post-body-title'})
+    subtitulo = noticia.find('a', attrs={'class': 'feed-post-body-title'})
+    if (subtitulo):
+        print("Subtitulo : " + subtitulo.text)
 
-#print(noticia.prettify())
+        #print(noticia.prettify())
 
-print("Titulo : " + titulo.text)
+    print("Titulo : " + titulo.text)
 
-print("Subtitulo : " + subtitulo.text)
+    
